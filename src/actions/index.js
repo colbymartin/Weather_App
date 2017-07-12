0f5dda01b956ee25ce872bd1ed48b1465d856a5f
Colby Martin <colbymartin@Colbys-MacBook-Air.local>
2017-07-12 15:29:47 -0400

    making API request and receiving network response

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '7422a40fcd920a358c61c282f5867464';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// Do this to keep action types consistent between action creaters and action reducers
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+    
+    return {
+        type: FETCH_WEATHER,
+        payload: request,
+    }
+}
